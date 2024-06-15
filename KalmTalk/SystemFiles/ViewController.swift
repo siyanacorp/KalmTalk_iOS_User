fe283a3afda468cb0b4974ba9d652745999845b2
Rahul-Dis <147652902+KharwarRahul@users.noreply.github.com>
2024-06-15 11:24:49 +0530

    Initial Setup

---
+++ KalmTalk/SystemFiles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KalmTalk
+//
+//  Created by Rahul Kharwar on 15/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // TODO: DEINIT
+    deinit{
+        print("   ViewController has been REMOVED...!")
+    }
+
+    // MARK: - ACTIONS
+}
+
