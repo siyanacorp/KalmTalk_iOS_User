bc25583b61b0e9edc380582efc11631202d58d29
Jogender866 <jogendersingh866@gmail.com>
2024-06-16 12:05:45 +0530

    Swiftlint added Testing remain

--- KalmTalk/SystemFiles/ViewController.swift
+++ KalmTalk/SystemFiles/ViewController.swift
@@ -12,13 +12,25 @@ class ViewController: UIViewController {
     // MARK: - PROPERTIES
     // MARK: - VIEW LIFE CYCLE METHODS
     // TODO: VIEW DID LOAD METHOD
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view, typically from a nib.
+
+      let button = UIButton(type: .roundedRect)
+      button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+      button.setTitle("Test Crash", for: [])
+      button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+      view.addSubview(button)
+  }
 
     // TODO: DEINIT
     deinit{
         print("   ViewController has been REMOVED...!")
     }
     // MARK: - ACTIONS
+  @IBAction func crashButtonTapped(_ sender: AnyObject) {
+      let numbers = [0]
+      let _ = numbers[1]
+  }
 }
