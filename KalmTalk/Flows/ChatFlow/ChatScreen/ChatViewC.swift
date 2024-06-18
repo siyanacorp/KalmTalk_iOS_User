52bc8dac3c0de27421ad5a60bad84332dd36095d
Jogender866 <jogendersingh866@gmail.com>
2024-06-18 12:47:08 +0530

    3 TabBars are added with items Home,Therapy and chat

---
+++ KalmTalk/Flows/ChatFlow/ChatScreen/ChatViewC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatViewC.swift
+//  KalmTalk
+//
+//  Created by Jogender Singh on 18/06/24.
+//
+
+import UIKit
+
+class ChatViewC: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // TODO: DEINIT
+    deinit{
+        print("ChatViewC has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
+
+}
---
+++ KalmTalk/Flows/TherapyFlow/TherapyScreen/TherapyViewC.swift
@@ -0,0 +1,25 @@
+//
+//  TherapyViewC.swift
+//  KalmTalk
+//
+//  Created by Jogender Singh on 18/06/24.
+//
+
+import UIKit
+
+class TherapyViewC: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // TODO: DEINIT
+    deinit{
+        print("TherapyViewC has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
+}
